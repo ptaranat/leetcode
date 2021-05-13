9cd4fb51754ee96a7b16440bd1f64bfa293fb88d
Panat Taranat <panat.taranat@gmail.com>
2021-05-12 23:48:55 -0400

    [0349][cpp] use erase to check for common elements

--- cpp/intersection_of_two_arrays.cpp
+++ cpp/intersection_of_two_arrays.cpp
@@ -2,15 +2,12 @@
 using namespace std;
 
 vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-  set<int> A, B, intersect;
-  for (int x : nums1) {
-    A.insert(x);
-  }
+  set<int> s(nums1.begin(), nums1.end());
+  vector<int> out;
   for (int x : nums2) {
-    B.insert(x);
+    // If we can remove a key from A that appears in B,
+    // then x is the common element
+    if (s.erase(x)) out.push_back(x);
   }
-  set_intersection(A.begin(), A.end(), B.begin(), B.end(),
-                   inserter(intersect, intersect.begin()));
-
-  return vector<int>(intersect.begin(), intersect.end());
+  return out;
 }
